97bdce7b9cee202371a45fb94e19c04691572000
vesania-sofia-tobar <97969064+vesania-sofia@users.noreply.github.com>
2022-01-29 17:32:13 -0600

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "calculadora basica" << endl;
+    cout << "empecemos :)" << endl;
+    cout << "ingrese el valor #1: ";
+    cin  >> num1;
+    cout << "ingrese el valor #2: ";
+    cin  >> num2;
+    cout << "el resulatado de la suma es: "<<num1+num2<<endl;
+    cout << "el resulatado de la resta es: "<<num1-num2<<endl;
+    cout << "el resulatado de la multiplicacion es: "<<num1*num2<<endl;
+    cout << "el resulatado de la division es: "<<num1/num2<<endl;
+    cout << "gracias, hasta la proxima" << endl;
+
+    return 0;
+}
